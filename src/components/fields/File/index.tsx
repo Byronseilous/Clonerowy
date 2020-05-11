fb0724f246bb7cb93455b976a50d0d4941c32363
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2020-05-11 15:38:01 +0300

    feat: Implement TypeScript component in (index.tsx)

---
+++ src/components/fields/File/index.tsx
@@ -0,0 +1,32 @@
+import { lazy } from "react";
+import { IFieldConfig, FieldType } from "@src/components/fields/types";
+import withRenderTableCell from "@src/components/Table/TableCell/withRenderTableCell";
+
+import FileIcon from "@mui/icons-material/AttachFile";
+import DisplayCell from "./DisplayCell";
+
+const EditorCell = lazy(
+  () => import("./EditorCell" /* webpackChunkName: "EditorCell-File" */)
+);
+const SideDrawerField = lazy(
+  () =>
+    import("./SideDrawerField" /* webpackChunkName: "SideDrawerField-File" */)
+);
+
+export const config: IFieldConfig = {
+  type: FieldType.file,
+  name: "File",
+  group: "File",
+  dataType:
+    "{ downloadURL: string; lastModifiedTS: number; name: string; type: string; ref: string; }[]",
+  initialValue: [],
+  icon: <FileIcon />,
+  description: "File uploaded to Firebase Storage. Supports any file type.",
+  TableCell: withRenderTableCell(DisplayCell, EditorCell, "inline", {
+    disablePadding: true,
+  }),
+  SideDrawerField,
+};
+export default config;
+
+export { FileIcon };
