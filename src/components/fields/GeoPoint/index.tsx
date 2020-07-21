be816b9b50cee91fd04a608f02e392ece2ef0e3a
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2020-07-21 21:20:03 +0300

    feat: Implement TypeScript component in (index.tsx)

---
+++ src/components/fields/GeoPoint/index.tsx
@@ -0,0 +1,41 @@
+import { lazy } from "react";
+import { GeoPoint } from "firebase/firestore";
+import { IFieldConfig, FieldType } from "@src/components/fields/types";
+import withRenderTableCell from "@src/components/Table/TableCell/withRenderTableCell";
+
+import GeoPointIcon from "@mui/icons-material/PinDropOutlined";
+import DisplayCell from "./DisplayCell";
+
+const SideDrawerField = lazy(
+  () =>
+    import(
+      "./SideDrawerField" /* webpackChunkName: "SideDrawerField-GeoPoint" */
+    )
+);
+
+export const config: IFieldConfig = {
+  type: FieldType.geoPoint,
+  name: "GeoPoint (Alpha)",
+  group: "Numeric",
+  dataType: "{latitude:number; longitude:number}",
+  initialValue: {},
+  icon: <GeoPointIcon />,
+  description: "Geo point is represented as latitude/longitude pair.",
+  TableCell: withRenderTableCell(DisplayCell, SideDrawerField, "popover", {
+    popoverProps: { PaperProps: { sx: { p: 1, pt: 0 } } },
+  }),
+  SideDrawerField,
+  csvImportParser: (value: string) => {
+    try {
+      const { latitude, longitude } = JSON.parse(value);
+      if (latitude && longitude) {
+        return new GeoPoint(latitude, longitude);
+      }
+      throw new Error();
+    } catch (e) {
+      console.error("Invalid GeoPoint value");
+      return null;
+    }
+  },
+};
+export default config;
