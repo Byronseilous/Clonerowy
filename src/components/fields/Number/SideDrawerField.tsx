2487e649fa9645f1f838c5cbd1cc3c798363f9f3
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2020-09-03 07:39:42 +0300

    feat: Implement TypeScript component in (SideDrawerField.tsx)

---
+++ src/components/fields/Number/SideDrawerField.tsx
@@ -0,0 +1,28 @@
+import { ISideDrawerFieldProps } from "@src/components/fields/types";
+
+import { TextField } from "@mui/material";
+import { getFieldId } from "@src/components/SideDrawer/utils";
+
+export default function Number_({
+  column,
+  value,
+  onChange,
+  onSubmit,
+  disabled,
+}: ISideDrawerFieldProps) {
+  return (
+    <TextField
+      variant="filled"
+      fullWidth
+      margin="none"
+      onChange={(e) => onChange(Number(e.target.value))}
+      onBlur={onSubmit}
+      value={value}
+      id={getFieldId(column.key)}
+      label=""
+      hiddenLabel
+      disabled={disabled}
+      type="number"
+    />
+  );
+}
